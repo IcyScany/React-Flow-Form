70f4868bdf9470a93a4f71d9ce5c4a406ed40edb
shuaicanyu <canyu.shuai@bl.com>
2026-02-06 16:18:50 +0800

    feat: 新增表单物料库Materials

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
 import FormRenderer from './components/Renderer'
-import { Button } from 'antd'
+import { Button, Space } from 'antd'
 import { useFormState } from './store/useFormState'
+import { Materials } from './config/materials'
+import { nanoid } from 'nanoid'
 
 const App = () => {
     const { schema, addComponent } = useFormState()
 
-    const handleAdd = () => {
+    const handleAddMaterial = (material: any) => {
+        const uniqueId = nanoid(8)
         addComponent({
-            id: `input_${Date.now()}`,
-            type: 'input',
-            label: '新输入框',
-            name: `field_${Date.now()}`,
+            ...material,
+            id: uniqueId,
+            name: `${material.type}_${uniqueId}`,
         })
     }
 
     return (
         <div style={{ display: 'flex', height: '100vh' }}>
-            <div style={{ width: 250, borderRight: '1px solid #ddd', padding: 20 }}>
-                <Button block onClick={handleAdd}>
-                    添加输入框
-                </Button>
+            <div className={'left-panel'}>
+                <h3>组件库</h3>
+                <Space orientation="vertical" style={{ width: '100%' }}>
+                    {Materials.map((m) => (
+                        <Button
+                            key={m.type}
+                            block
+                            onClick={() => handleAddMaterial(m)}
+                        >
+                            {m.title}
+                        </Button>
+                    ))}
+                </Space>
             </div>
 
             <div style={{ flex: 1, padding: 40, backgroundColor: '#f5f5f5' }}>
-                <div style={{ background: '#fff', padding: 20, minHeight: '100%' }}>
+                <div
+                    style={{
+                        background: '#fff',
+                        padding: 20,
+                        minHeight: '100%',
+                    }}
+                >
                     <FormRenderer items={schema.items} />
                 </div>
             </div>
